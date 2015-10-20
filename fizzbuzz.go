ae32c3889c4ef696d67bc07d12aa4edc7ada3080
Clare Glinka <clare.glinka@returnpath.com>
2015-10-20 17:56:33 -0500

    Add fizzbuzz in Go using if statements.

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func switchFizz() {
 	for i := 1; i <= 50; i += 1 {
 		switch {
 		case i%3 == 0 && i%5 == 0:
@@ -15,4 +15,25 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+}
+
+func ifFizz() {
+	for i := 1; i <= 50; i += 1 {
+		p := ``
+		if i%3 == 0 {
+			p = "Fizz "
+		}
+		if i%5 == 0 {
+			p = p + "Buzz"
+		}
+		if p == `` {
+			p = fmt.Sprintf("%d", i)
+		}
+		fmt.Println(p)
+	}
+}
+
+func main() {
+	switchFizz()
+	ifFizz()
 }
